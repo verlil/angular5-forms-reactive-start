a44c468caad5638df38b0788c8330efc683891bb
verlil <vernigoralilia@gmail.com>
2018-06-19 02:21:28 +0300

    custom validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'emailAdress': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('female'),
@@ -29,5 +30,17 @@ export class AppComponent implements OnInit {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
     // (<FormArray>...) - this is a FormArray type explanation
+  }
+
+  // validator - is just a function, that is executing by Angular automatically
+  // when it checks the validity of form control
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    //example of this function return:  {'nameIsForbidden': true}    (where nameIsForbidden is string)
+    // you can't use 'this.' in validator function by default, so
+    // don't forget to add '.bind(this)' in place of using this validator
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
