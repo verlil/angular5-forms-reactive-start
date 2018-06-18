941cf8b04bcdd14eec599001464ef48ecd89260d
verlil <vernigoralilia@gmail.com>
2018-06-19 02:00:26 +0300

    reactive adding inputs to use data in array FormArray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +16,18 @@ export class AppComponent implements OnInit {
         'username': new FormControl(null, Validators.required),
         'emailAdress': new FormControl(null, [Validators.required, Validators.email]),
       }),
-      'gender': new FormControl('female')
+      'gender': new FormControl('female'),
+      'hobbies': new FormArray([])
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+    // (<FormArray>...) - this is a FormArray type explanation
   }
 }
